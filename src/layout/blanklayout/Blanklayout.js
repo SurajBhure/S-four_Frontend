4a0afc9b3cef84a762b3f7ae5063371d60d722fd
SurajBhure <surajkumarwebdesigner@gmail.com>
2022-11-10 14:27:28 +0530

    file update 1st time

---
+++ src/layout/blanklayout/Blanklayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Headers from './Headers'
+
+const Blanklayout = () => {
+  return (
+    <div><Headers /></div>
+  )
+}
+
+export default Blanklayout
---
+++ src/layout/fulllayout/Fulllayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fulllayout = () => {
+  return (
+    <div>Fulllayout</div>
+  )
+}
+
+export default Fulllayout
